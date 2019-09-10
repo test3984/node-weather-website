515f59994454c95e6774203010482f428c201d3f
test3984 <godwarsow@gmail.com>
2019-09-10 02:38:57 +0200

    Removed unnecessary console log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side js file');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.getElementById('message-1');
